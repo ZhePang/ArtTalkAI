d060c69ab2107b84c87b267545df642d47f88047
Jesse Chong <jlc2332@columbia.edu>
2023-12-16 16:55:18 -0800

    Added author to UX and cleaned up old unused code

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -39,30 +39,6 @@ class ArtsController < ApplicationController
     # default: render 'new' template
   end
 
-#   def create
-#     @art = Art.create!(art_params)
-#     flash[:notice] = "#{@art.title} was successfully created."
-#     redirect_to arts_path
-#   end
-
-#   def edit
-#     @art = Art.find params[:id]
-#   end
-
-#   def update
-#     @art = Art.find params[:id]
-#     @art.update_attributes!(art_params)
-#     flash[:notice] = "#{@art.title} was successfully updated."
-#     redirect_to art_path(@art)
-#   end
-
-#   def destroy
-#     @art = Art.find(params[:id])
-#     @art.destroy
-#     flash[:notice] = "Art '#{@art.title}' deleted."
-#     redirect_to arts_path
-#   end
-
   private
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,7 +13,6 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
     when /^the (ArtTalkAI )?home\s?page$/ then '/arts'
     when /^the detailed page of the art piece of the day$/ then art_path(Art.find(DateTime.now.strftime('%d').to_i % (Art.count) + 1).id)
     when /^the gallery page$/ then '/arts/gallery'
