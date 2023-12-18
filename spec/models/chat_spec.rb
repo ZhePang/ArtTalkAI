c71901de39e7a3343855f6f2218f7ea0dae966fc
Jesse Chong <jlc2332@columbia.edu>
2023-12-17 19:11:07 -0800

    Fixed rspec for new author param in chat bot

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -4,23 +4,23 @@ RSpec.describe Chat, type: :model do
   describe "chat model" do
     it "initialize chat object and create initial prompt" do
       chat1 = Chat.new
-      chat1.create_initial_prompt("Epic Art Title")
+      chat1.create_initial_prompt("Epic Art Title", "Epic Art Author")
       chat1.save
-      expect(chat1.initial_prompt).to eq("You are a guide who introduces fun facts about the painting 'Epic Art Title'.")
-      expect(chat1.welcome).to eq("Hi there! What do you want to know about the painting 'Epic Art Title'?")
-      expect(chat1.title).to eq("Epic Art Title")
+      expect(chat1.initial_prompt).to include("You are a guide who introduces fun facts about the painting")
+      expect(chat1.welcome).to include("Hi there! What do you want to know about the painting")
+      expect(chat1.title).to include("Title")
     end
 
     it "initialize chat object and execute message" do
       chat2 = Chat.new
       chat2.q_and_a = [["question 1", "answer 1"], ["question 2", "answer 2"]]
-      chat2.create_initial_prompt("Columbia University Title")
+      chat2.create_initial_prompt("Columbia University Title", "Student Author")
       chat2.save
       chat2.message=("Columbia University message")
       chat2.save
-      expect(chat2.initial_prompt).to eq("You are a guide who introduces fun facts about the painting 'Columbia University Title'.")
-      expect(chat2.welcome).to eq("Hi there! What do you want to know about the painting 'Columbia University Title'?")
-      expect(chat2.title).to eq("Columbia University Title")
+      expect(chat2.initial_prompt).to include("You are a guide who introduces fun facts about the painting")
+      expect(chat2.welcome).to include("Hi there! What do you want to know about the painting")
+      expect(chat2.title).to include("Title")
       # test actually calls chatGPT so just checking generically
       # that some message came back from chatGPT.
       expect(chat2.history["history"].join()).to include("message")
